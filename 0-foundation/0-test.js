5425a4026eb1e9b265bddeb5169fac8c8f4af39b
Sani Abubakar Adam <Athernee@gmail.com>
2023-11-15 08:25:24 +0000

    Added: Initial files

---
+++ 0-foundation/0-test.js
@@ -0,0 +1,10 @@
+
+const os = require('os');
+
+//info about current user
+console.log(os.userInfo());
+
+// uptime of the system
+console.log("System up for: "+os.uptime() +" seconds");
+console.log('System up for: ' + os.uptime() + ' seconds');
+
